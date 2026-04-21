a27df3643e38e4eb1b05150cf47d3d52bdc8a924
sam-ayosph <sbracamonte.ayosph@gmail.com>
2026-04-21 21:57:04 +0800

    [FEAT] dynamic login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,11 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutterlookfor/main.dart';
-
-
-
 
 void main() {
   testWidgets('LookFor app loads', (WidgetTester tester) async {
-    // Build the app
     await tester.pumpWidget(const LookForApp());
-
-    // Allow routing & animations to settle
     await tester.pumpAndSettle();
 
-    // ✅ Verify that login screen content appears
     expect(find.text('LOG IN'), findsOneWidget);
     expect(find.text('Look'), findsWidgets);
     expect(find.text('For'), findsWidgets);
